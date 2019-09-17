8434daa9e5c3a636d7104246b8eefc8292a39193
RandeepKaur <randipk@gmail.com>
2019-09-17 09:19:38 +0530

    [Randeep]Add. Validations for Books Count value

--- src/main/java/com/example/biblioteca/BibliotecaController.java
+++ src/main/java/com/example/biblioteca/BibliotecaController.java
@@ -1,13 +1,15 @@
 package com.example.biblioteca;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.format.annotation.NumberFormat;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import java.util.List;
 
@@ -19,7 +21,10 @@ public class BibliotecaController {
     private BibliotecaService bibliotecaService;
 
     @GetMapping("/books")
-    List<Book> getBooksByCount(@RequestParam(value = "booksCount", required = false) @Positive Long booksCount)
+    List<Book> getBooksByCount(@Valid @RequestParam(value = "booksCount", required = false)
+                               @Positive
+                               @NumberFormat(style = NumberFormat.Style.NUMBER)
+                                       Long booksCount)
             throws NoBooksFoundException {
         if (booksCount == null) {
             return bibliotecaService.getAllBooks();
