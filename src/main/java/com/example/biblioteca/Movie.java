b6dbfdecf9ff812bd09f7637084b8df7849ba430
Merlin <merlin.p.varghese@gmail.com>
2019-09-18 21:41:49 +0530

    [Merlin]Add. Adding entity and repository for movies feature

---
+++ src/main/java/com/example/biblioteca/Movie.java
@@ -0,0 +1,50 @@
+package com.example.biblioteca;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Movie {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @JsonProperty
+  private final Long id;
+
+  @JsonProperty
+  private final String name;
+
+  @JsonProperty
+  private final String year;
+
+  @JsonProperty
+  private final String director;
+
+  @JsonProperty
+  private final String rating;
+
+  public Movie() {
+    this.id = null;
+    this.name = null;
+    this.year = null;
+    this.director = null;
+    this.rating = null;
+  }
+
+  @JsonIgnore
+  Long getId() {
+    return id;
+  }
+  public Movie(long id, String name,String year, String director, String rating) {
+    this.id = id;
+    this.name = name;
+    this.year = year;
+    this.director = director;
+    this.rating = rating;
+  }
+}
